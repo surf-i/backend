ec3f234c70df18a248b8367102e5ba1e209335a3
Juan Romero <juan.romero1201@gmail.com>
2022-04-18 20:52:53 -0500

    Agregada variable de entorno

--- SurfiBack/settings.py
+++ SurfiBack/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'SurfiBack.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # URL de la conexión a MongoDB
-DB_URI = ""
+DB_URI = os.environ["MONGO_KEY"]
 
 DATABASES = {
     'default': {
