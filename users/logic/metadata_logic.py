eabbaf1908d5c6a142f0276c75bf8efb34196a08
Juan <juan.romero1201@gmail.com>
2022-04-21 10:41:31 -0500

    Testing

--- users/logic/metadata_logic.py
+++ users/logic/metadata_logic.py
@@ -12,7 +12,8 @@ def update_metadata(review, website):
 
     category_metadata = wl.get_website_category_metadata(website.id)
     if category_metadata:
-        category_metadata = category_metadata.filter(tipo=review["categoria"])
+        print(category_metadata)
+        category_metadata = category_metadata.get(tipo=review["categoria"])
         category_metadata.cantReviews += 1
     else:
         category_metadata = wl.create_category_metadata(review, website)
