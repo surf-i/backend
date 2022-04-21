97b5875d3ee0c4fbf90f2502b80f8645e6561980
Juan <juan.romero1201@gmail.com>
2022-04-21 08:07:24 -0500

    Added backend to hosts

--- SurfiBack/settings.py
+++ SurfiBack/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ["DJANGO_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backend']
 
 SECURE_SSL_REDIRECT = False
 SECURE_HSTS_SECONDS = False
