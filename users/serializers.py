6375242d5e8861e4e18e144f95d41c66d3aba56f
Juan <juan.romero1201@gmail.com>
2022-04-22 10:21:59 -0500

    Fixed review serializer

--- users/serializers.py
+++ users/serializers.py
@@ -4,9 +4,9 @@ from .models import Review
 class ReviewSerializer(serializers.ModelSerializer):
     calificacion = serializers.FloatField(min_value=0, max_value=5)
     gradoVeracidad = serializers.FloatField(min_value=0, max_value=99)
-    calificacionDisenoPromedio = serializers.FloatField(min_value=0, max_value=99, allow_null=True)
-    calificacionUsabilidadPromedio = serializers.FloatField(min_value=0, max_value=99, allow_null=True)
-    
+    calificacionDiseno = serializers.FloatField(min_value=0, max_value=99, allow_null=True)
+    calificacionUsabilidad = serializers.FloatField(min_value=0, max_value=99, allow_null=True)
+
     class Meta:
         model = Review
         fields = '__all__'
