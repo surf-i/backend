f9e42d71e06de0ca295955bfba2e142aa127bb1c
Juan <juan.romero1201@gmail.com>
2022-04-22 10:30:29 -0500

    Fixed category metadata not saving bug

--- users/logic/metadata_logic.py
+++ users/logic/metadata_logic.py
@@ -1,5 +1,5 @@
 from websites.logic import websites_logic as wl
-from websites.models import ReviewMetadata, DatosCategoria
+from websites.models import ReviewMetadata
 def update_metadata(review, website):
     try:
         metadata = wl.get_website_review_metadata(website.id)
@@ -18,6 +18,7 @@ def update_metadata(review, website):
     if category_metadata:
         category_metadata = category_metadata.get(tipo=review["categoria"])
         category_metadata.cantReviews += 1
+        category_metadata.save()
     else:
         category_metadata = wl.create_category_metadata(review, website)
     wl.update_website_metadata(website, metadata)
