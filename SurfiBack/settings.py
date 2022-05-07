6beaccec7ccbfd727d277e99ee2001ce7394b8b8
Juan Romero <juan.romero1201@gmail.com>
2022-05-07 18:34:49 -0500

    Commented email stuff

--- SurfiBack/settings.py
+++ SurfiBack/settings.py
@@ -160,13 +160,13 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "noreply.surfi@gmail.com"
-EMAIL_HOST_PASSWORD = os.environ["EMAIL_PASSWORD"]
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_HOST_USER = "noreply.surfi@gmail.com"
+#MAIL_HOST_PASSWORD = os.environ["EMAIL_PASSWORD"]
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_USE_SSL = False
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
