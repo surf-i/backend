0b012296831635976e7f00f510bbc03253b41f30
Juan Romero <juan.romero1201@gmail.com>
2022-05-11 11:28:34 -0500

    Fixing urls

--- websites/urls.py
+++ websites/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.multiple_website_view, name="websites_view"),
     path('', views.single_website_view, name='website_view'),
 ]
--- websites/views.py
+++ websites/views.py
@@ -8,23 +8,8 @@ from .logic import websites_logic as wl
 from .models import Website
 from django.db import transaction
 
-
-@api_view(['GET', 'POST', 'PUT'])
-def multiple_website_view(request):
-    if request.method == "GET":
-        websites = wl.get_websites()
-        website_serializer = WebsiteSerializer(websites, many=True)
-        return JsonResponse(website_serializer.data, safe=False)
-    elif request.method == "POST":
-        website_dto = JSONParser().parse(request)
-        website_serializer = WebsiteSerializer(data=website_dto)
-        if website_serializer.is_valid():
-            website_serializer.save()
-            return JsonResponse(website_serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 @transaction.non_atomic_requests
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def single_website_view(request):
     if request.method == "GET":
         request_url= request.GET.get("url")
@@ -35,5 +20,12 @@ def single_website_view(request):
             return JsonResponse({"error": "Website does not exist"}, status=status.HTTP_404_NOT_FOUND)
         website_serializer = WebsiteSerializer(website)
         return JsonResponse(website_serializer.data, safe=False)
-            
+    elif request.method == "POST":
+        website_dto = JSONParser().parse(request)
+        website_serializer = WebsiteSerializer(data=website_dto)
+        if website_serializer.is_valid():
+            website_serializer.save()
+            return JsonResponse(website_serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
+    
     return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
