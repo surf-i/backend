536db913e4603b480cf11d70a03ab2a3735764c4
David Sequera <davidsequerads@gmail.com>
2022-05-12 13:46:47 -0500

    Added possible get multiple implementation

--- websites/urls.py
+++ websites/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.single_website_view, name='website_view'),
+    path('/multiple', views.multiple_website_view, name='get_multiple_websites')
 ]
--- websites/views.py
+++ websites/views.py
@@ -1,4 +1,5 @@
 
+from email.mime import application
 from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser 
@@ -29,3 +30,17 @@ def single_website_view(request):
         return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
     
     return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+def multiple_website_view(request):
+    if request.method == "GET":
+        request_urls = request.GET.getlist('url')
+        if not request_urls:
+            return JsonResponse({"error": "Missing url or incorrect format on request parameters"}, status=status.HTTP_400_BAD_REQUEST)
+        website_list = list()
+
+        for website in request_urls:
+            raw_website = wl.get_website_by_url(website)
+            website_list.append(WebsiteSerializer(raw_website).data)
+
+        return JsonResponse(website_list, safe=False)
