2f9383711ccb06e3c779d2ae3673cc8e4c572935
Juan Romero <juan.romero1201@gmail.com>
2022-05-15 13:24:36 -0500

    Added auth to post

--- websites/views.py
+++ websites/views.py
@@ -2,11 +2,13 @@
 from email.mime import application
 from django.http.response import JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser 
 from .serializers import WebsiteSerializer
 from rest_framework import status
 from .logic import websites_logic as wl
+from users.logic import users_logic as ul
 from django.db import transaction
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 @transaction.non_atomic_requests
 @api_view(["GET", "POST"])
@@ -21,19 +23,28 @@ def single_website_view(request):
         website_serializer = WebsiteSerializer(website)
         return JsonResponse(website_serializer.data, safe=False)
     elif request.method == "POST":
-        request.data["url"] = "https://" + request.data["url"]
-        request.data["categoria"] = "NOT RATED"
-        request.data["gradoVeracidadPromedio"] = 0.0
-        request.data["calificacionPromedio"] = 0.0
-        request.data["autor"] = None
-        request.data["fecha"] = None
-        request.data["disenoPromedio"] = 0.0
-        request.data["usabilidadPromedio"] = 0.0
-        website_serializer = WebsiteSerializer(data=request.data)
-        if website_serializer.is_valid():
-            website_serializer.save()
-            return JsonResponse(website_serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
+        try:
+            auth = request.META.get("HTTP_AUTHORIZATION")
+            if not auth:
+                return JsonResponse({"ERROR": "User must be authenticated"}, status=status.HTTP_401_UNAUTHORIZED)
+            else:
+                auth = auth.split(" ")[1]
+                ul.get_user_by_token(auth)
+                request.data["url"] = "https://" + request.data["url"]
+                request.data["categoria"] = "NOT RATED"
+                request.data["gradoVeracidadPromedio"] = 0.0
+                request.data["calificacionPromedio"] = 0.0
+                request.data["autor"] = None
+                request.data["fecha"] = None
+                request.data["disenoPromedio"] = 0.0
+                request.data["usabilidadPromedio"] = 0.0
+                website_serializer = WebsiteSerializer(data=request.data)
+                if website_serializer.is_valid():
+                    website_serializer.save()
+                    return JsonResponse(website_serializer.data, status=status.HTTP_201_CREATED)
+                return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except (Token.DoesNotExist, User.DoesNotExist):
+            return JsonResponse({"ERROR": "Token bad request"}, status=status.HTTP_400_BAD_REQUEST)       
     
     return JsonResponse(website_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
